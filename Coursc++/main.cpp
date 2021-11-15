87adac16ec38e3a075ae60313f486fa5e1520f43
adrien <a.dubost@safetyn.com>
2021-11-15 11:52:21 +0100

    GuessNumber

--- Coursc++/main.cpp
+++ Coursc++/main.cpp
@@ -3,6 +3,17 @@
 using namespace std;
 int main()
 {
+    //Ceci est un commentaire
     cout << " Bienvenue au Juste Prix ! " << endl;
+    //Déclaration Int
+    int integer(43);
+    auto numero(4'000'000'000'000'000);
+    cout<<integer<<endl;
+    cout << numero << endl;
+    int GuessNumber{0};
+    cin >> GuessNumber;
+    cout << GuessNumber << endl;
+
     return 0;
+
 }
