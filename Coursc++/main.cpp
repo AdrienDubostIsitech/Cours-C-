653bb1576aeca95d5e0e1cc4d2acaa65611e84fc
adrien <a.dubost@safetyn.com>
2021-11-15 12:33:39 +0100

    Juste Prix Loop with Try and Borne

--- Coursc++/main.cpp
+++ Coursc++/main.cpp
@@ -5,34 +5,43 @@ int main()
 {
     int GuessNumber{2};
     int PlayerAnswer{0};
+    int Try{0};
+    bool win = false;
 
     cout<< "Bienvenue au Juste Prix" << endl;
     cout<< "Devinez le prix d'un pain au chocolat : " << endl;
 
-
-    while(PlayerAnswer != GuessNumber)
+    do
     {
+        Try++;
         cin >> PlayerAnswer;
-        if(PlayerAnswer < GuessNumber)
+        if(PlayerAnswer < 10000)
         {
-            cout << "C'est plus ! " << endl;
+            if (PlayerAnswer < 0)
+            {
+                cout<< " Un chiffre inferieur à zero a etait soumis, vous alle etre redirige hors du jeu " << endl;
+                win = true;
+            }
+            else if(PlayerAnswer < GuessNumber)
+            {
+                cout << "C'est plus ! " << endl;
+            }
+            else if(PlayerAnswer > GuessNumber)
+            {
+                cout << "C'est moins !" << endl;
+            }
+            else if( PlayerAnswer == GuessNumber)
+            {
+                cout<< " C'est gagne !" << endl;
+                win = true;
+            }
         }
-        else if(PlayerAnswer > GuessNumber)
+        else
         {
-            cout << "C'est moins !" << endl;
-        }
-        else if( PlayerAnswer == GuessNumber)
-        {
-            cout<< " C'est gagne !" << endl;
+            cout << "Ce nombre est trop grand pour etre considere" << endl;
         }
     }
+    while(win == false);
 
-    //if(PlayerAnswer == GuessNumber)
-    //{
-
-    //}
-    //else
-    //{
-       // cout<< " C'est perdu !"<< endl;
-    //}
+    cout << "Votre nombre de tentatives a ete de : "<< Try << endl;
 }
