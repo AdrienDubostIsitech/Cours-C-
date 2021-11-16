8362d4eb0e2828745e84f74d1236a620ec443cc9
adrien <a.dubost@safetyn.com>
2021-11-16 14:05:27 +0100

    Ajout d'un choix random de mots à travers la liste de mot

--- LePendu/main.cpp
+++ LePendu/main.cpp
@@ -1,16 +1,23 @@
 #include <iostream>
 #include <array>
+#include <time.h>
 #include "WordList.h"
 
 using namespace std;
 
 int main()
 {
+    srand(time(NULL));
+    int ChooseWordIndex{0};
+    std::string ChoosenWord;
     array<std::string, NOMBRE_MOT> WordList {GestionWord, EntrepriseWord, VoitureWord, ChevalWord, CroquetteWord};
-
-    for(int i = 0;  i < sizeof(WordList) ; i++)
+    ChooseWordIndex = rand() % WordList.size();
+    for(int i = 0;  i < WordList.size() ; i++)
     {
             cout << WordList[i] << "    ";
     }
+    cout << endl;
+    cout << WordList[ChooseWordIndex] << endl;
+
     return 0;
 }
