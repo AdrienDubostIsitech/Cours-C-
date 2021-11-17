bd39f959cf46fe40aa9d549d6d901a1af2fdce9e
adrien <a.dubost@safetyn.com>
2021-11-17 12:46:56 +0100

    Fin du pendu avec commentaires et README + Class constantes dans Enter the Dungeon avec explication de ce que je n'ai pas pu faire

---
+++ EnterTheDungeon/Constantes.h
@@ -0,0 +1,11 @@
+#ifndef CONSTANTES_H_INCLUDED
+#define CONSTANTES_H_INCLUDED
+
+    //Fichier qui était censé contenir les constantes du Dungeon
+    // Les bornes supèrieur et infèrieur du Dungeon
+    //Je vooulais aussi créer une classe player qui aurait eu des points de vie
+    // des dégats et de l'or
+    //Il aurait pu ramasser de l'or avec les Room tresor et se battre dans les room ennemis
+    // Il y aurait eu un combat au tour par tour classique entre le joueur et l'ennemi
+
+#endif // CONSTANTES_H_INCLUDED
