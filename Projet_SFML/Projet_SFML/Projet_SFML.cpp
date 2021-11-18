1e92d5f78061ef7f135d50229c8f91ca6038dc14
adrien <a.dubost@safetyn.com>
2021-11-18 17:26:13 +0100

    SFML : creation de delta time // j'ai essayé de faire bouger le carré avec ZQSD mais j'ai pas eu le temps de faire des events

--- Projet_SFML/Projet_SFML/Projet_SFML.cpp
+++ Projet_SFML/Projet_SFML/Projet_SFML.cpp
@@ -2,23 +2,41 @@
 //
 
 #include <iostream>
+#include<ctime>
+#include <cstdlib>
 #include <SFML/Graphics.hpp>
+#include "SFML\System.hpp"
+#include "SFML\Window.hpp"
 
+int MoveSpeed{ 8 }; 
+static sf::Clock dtCLock; 
+float dt; 
+
+
+void UpdateDt() 
+{
+    dt = dtCLock.getElapsedTime().asSeconds(); 
+}
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
+    sf::RenderWindow window(sf::VideoMode(900, 900), "SFML works!");
+    sf::RectangleShape shape(sf::Vector2f(120, 120)); 
     shape.setFillColor(sf::Color::Green);
-
     while (window.isOpen())
     {
         sf::Event event;
         while (window.pollEvent(event))
         {
+            ; 
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Z)) 
+        {   
+            sf::RectangleShape shape2(sf::Vector2f(500, 600));
+            shape2.setFillColor(sf::Color::Red);
+            shape.setPosition(sf::Vector2f(0, MoveSpeed * dt)); 
+        }
         window.clear();
         window.draw(shape);
         window.display();
